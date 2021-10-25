943112774f691be191250eb5665f0672175ed9fb
Math Galvao <89107525+msgalvao@users.noreply.github.com>
2021-10-25 20:59:32 -0300

    Add files via upload
    Upei 4 atividades realizadas na aula 4 - 25.10.2021

---
+++ Aula.04/ex1.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+
+var x = 8; // atribuição
+const y = 6;
+let z = 2;
+
+console.log(y);
+
+let soma = x + y + z;
+let subtracao = x - y - z;
+let divisao = x / y;
+let multiplicacao = x * y;
+
+// console.log(soma);
+// console.log(subtracao);
+// console.log(divisao);
+// console.log(multiplicacao);
+
+let nome = "Matheus";
+let idade = 25; 
+
+
+console.log(nome+" - "+idade);
