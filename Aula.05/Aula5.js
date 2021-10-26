5027da65ea7ceb40f5d5f625f80302cb6da75975
Math Galvao <89107525+msgalvao@users.noreply.github.com>
2021-10-26 20:43:37 -0300

    Add files via upload

---
+++ Aula.05/Aula5.js
@@ -0,0 +1,13 @@
+function soma(x,y){//criar fução
+    return x + y; // retorna calculo
+    }
+    
+    console.log(soma(15,10)); //printei o retorno da funcao
+    
+    function pessoa(nome,idade){
+        return nome+" tem "+ idade; 
+    }
+        
+        console.log(pessoa("Matheus", 30)); 
+
+        
