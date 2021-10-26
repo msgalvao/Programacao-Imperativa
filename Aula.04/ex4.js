1a3e6ff9d05fd54ac85ba6efab8dd798d4750016
Math Galvao <89107525+msgalvao@users.noreply.github.com>
2021-10-25 21:00:22 -0300

    Add files via upload

---
+++ Aula.04/ex4.js
@@ -0,0 +1,12 @@
+
+
+
+let Nome = "Jose";
+let Sobrenome = "da Silva";
+let Idade = 27; 
+let Peso = 83.5;
+let Altura = 1.7;
+let Plano = "Ouro";
+let imc = Peso/(Altura*Altura);
+
+console.log(Nome+" tem "+Idade+" anos "+". Seu (Indice de Massa Corporal) é de"+imc+".");
