a793976107cbca2c016f797cb0ecea81e4239669
Math Galvao <89107525+msgalvao@users.noreply.github.com>
2021-11-03 19:25:16 -0300

    Add files via upload

---
+++ Aula.06/calculadora.js
@@ -0,0 +1,57 @@
+console.log ("---- Teste de Operações / Calculadora NIVEL I ----")
+
+console.log ("Ex 1")
+
+function adicionar (x ,y){
+    return (x + y)
+}
+console.log (adicionar (23,27))
+
+console.log ("Ex 2")
+
+function subtracao (x ,y){
+    return (x - y)
+}
+console.log (subtracao (100,50))
+
+console.log ("Ex 3")
+
+function multiplicacao (x ,y){
+    return (x * y)
+}
+console.log (multiplicacao (25,2))
+
+console.log ("Ex 4")
+
+function divisao (x ,y){
+    return (x / y)
+}
+console.log (divisao (100,2))
+
+
+console.log ("---- Teste de Operações / Calculadora NIVEL II ----")
+
+console.log (adicionar (8,7))
+console.log (subtracao (60,30))
+console.log (multiplicacao (9,5))
+console.log (divisao (100,0))
+
+console.log ("---- Teste de Operações / Calculadora NIVEL III ----")
+
+console.log ("Ex 1")
+
+function quadradoDoNumero (x){
+ return multiplicacao (x , x)
+
+}
+console.log (quadradoDoNumero (7))
+
+console.log ("Ex 2")
+
+function mediaDeTresNumeros (x, y, z) {
+     return (adicionar (x,y)+z)/3 
+
+}
+console.log (mediaDeTresNumeros (2 , 4, 6))
+
+console.log ("Ex 3")
